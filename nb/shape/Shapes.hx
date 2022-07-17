fe30e19a9951e49f5017077485e0185a617bce2e
NeroGM <Nero.GM@protonmail.com>
2022-07-17 20:17:39 +0200

    Shapes: makeUnion partly done

--- nb/shape/Shapes.hx
+++ nb/shape/Shapes.hx
@@ -1,6 +1,7 @@
 package nb.shape;
 
 using nb.ext.PointExt;
+using nb.ext.SegmentExt;
 
 class Shapes extends Object {
     public var shapes:Array<Shape> = [];
@@ -109,160 +110,51 @@ class Shapes extends Object {
         return res;
     }
 
-    private function makeUnion() {
-    //     var shapes = a;
-    //     var graph = new Graph();
-    //     var startingP:Point = null;
-    // //    graph.onConnect = (node1, node2) -> trace("connect : " + node1.p + "  ->  " + node2.p);
-    // //    trace(" ---------------- " + a);
-    //     if (shapes.length > 1) {
-    //         for (shape1 in shapes) {
-    //             if (shape1.type == POLYGON || shape1.type == CIRCLE) {
-    //                 var pol1:Polygon = null;
-    //                 if (shape1.type == POLYGON) pol1 = cast(shape1,Polygon);
-    //                 else {
-    //                     var c = cast(shape1,Circle);
-    //                     pol1 = Polygon.makeCircle(c.col.x,c.col.y,c.radius,0);
-    //                 }
-    //                 var segments1 = pol1.toSegments();
-    //                 var startingNode:nb.Graph.Node = null;
-    //                 var onNode:nb.Graph.Node = null;
-    //                 for (seg1 in segments1) {
-    //                     var p1 = new Point(seg1.x,seg1.y);
-    //                     var p2 = new Point(seg1.x+seg1.dx,seg1.y+seg1.dy);
-    //                     var sIntersections:Array<Point> = [];
-    //                     for (shape2 in shapes) if (shape2 != shape1) {
-    //                         if (shape2.type == POLYGON) {
-    //                             var pol2 = cast(shape2,Polygon);
-    //                             var segments2 = pol2.points.toSegments();
-    //                             for (seg2 in segments2) {
-    //                                 var p3 = new Point(seg2.x,seg2.y);
-    //                                 var p4 = new Point(seg2.x+seg2.dx,seg2.y+seg2.dy);
-    //                                 var intersection:Point = new Point();
-    //                                 var v = nb.phys.Collision.checkSegments(p1,p2,p3,p4,intersection);
-    //                                 //trace("(" + p1+","+p2+")     ("+p3+","+p4+")");
-    //                             //    trace(v);
-    //                                 if (v > 0) {
-    //                                     sIntersections.push(intersection);
-    //                                     //trace("inters:    (" + p1+","+p2+")     ("+p3+","+p4+")    " + intersection);
-    //                                 }
-    //                             }
-    //                         } else if (shape2.type == CIRCLE) {
-    //                             var cir2 = cast(shape2,Circle);
-    //                             var v = nb.phys.Collision.checkCircleSegment(seg1,cir2.col);
-    //                             // trace(v);
-    //                             if (v != null) for (inters in v) sIntersections.push(inters);
-    //                         } else throw "nah";
-    //                     }
+    public function makeUnion():Graph {
+        var graph = new Graph();
+        var checkedShapes:Array<Shape> = [];
+        var savedSegments:haxe.ds.Map<Int,Array<Segment>> = new haxe.ds.Map();
 
-    //                     if (seg1 == segments1[0]) {
-    //                         var node = graph.getNodeAtPoint(new Point(p1.x,p1.y));
-    //                         startingNode = onNode = node == null ? graph.addNode(new Point(p1.x,p1.y)) : node;
-    //                     }
-                        
-    //                     nb.ext.ArrayExt.quickSort(sIntersections,(a,b) -> {
-    //                         return a.sub(p1).length() < b.sub(p1).length();
-    //                     });
+        if (shapes.length < 2) return null;
 
-    //                     for (inters in sIntersections) {
-    //                         var node2 = graph.getNodeAtPoint(inters);
-    //                         // trace(inters + "   " + (node2 == null ? "null" : Std.string(node2.p)));
-    //                         if (node2 == null) node2 = graph.addNode(inters.clone());
-    //                         graph.connect(node2, [onNode]);
-    //                         onNode = node2;
-    //                     }
+        for (shape1 in shapes) {
+            if (shape1 is Polygon) {
+                var pol1 = cast(shape1,Polygon);
+                if (savedSegments[shape1.objId] == null) savedSegments[shape1.objId] = pol1.toSegments();
+                var segments1:Array<Segment> = savedSegments[shape1.objId];
+                for (i in 0...segments1.length) {
+                    var seg1 = segments1[i];
+                    var p1 = seg1.getA();
+                    var p2 = seg1.getB();
+                    var nodeP1 = i == 0 ? graph.addNode(p1) : graph.getNodeAtPoint(p1);
+                    var nodeP2 = i == segments1.length-1 ? graph.getNodeAtPoint(p2) : graph.addNode(p2);
+                    if (checkedShapes.length == 0) graph.connect(nodeP1,[nodeP2]);
+                    else for (shape2 in checkedShapes) {
+                        if (shape2 is Polygon) {
+                            var pol2 = cast(shape2,Polygon);
+                            if (savedSegments[shape2.objId] == null) savedSegments[shape2.objId] = pol2.toSegments();
+                            var segments2:Array<Segment> = savedSegments[shape2.objId];
 
-    //                     var p = new Point(seg1.x+seg1.dx,seg1.y+seg1.dy);
-    //                     var toNode:nb.Graph.Node = graph.getNodeAtPoint(p);
-    //                     if (toNode == null) toNode = graph.addNode(p);
-    //                     graph.connect(toNode, [onNode]);
-    //                     onNode = toNode;
-
-    //                     var farthestP = pol1.getSupportPoint(new Point(1,1));
-    //                     if (startingP == null || startingP.length() < farthestP.length()) startingP = farthestP;
-    //                     //  trace("e      " + pol1.type +"     " + pol1.subType);
-    //                 }
-    //             } else throw "??";
-    //         }
-    //         // trace(startingP);
-
-    //         // Connect nodes
-    //         var unvisitedNodes:Array<nb.Graph.Node> = graph.allNodes.copy();
-    //         var visitedNodes:Array<nb.Graph.Node> = [];
-    //         var lastNode:nb.Graph.Node = null;
-    //         var onNode:nb.Graph.Node = null;  for (n in graph.allNodes) if (n.p.equalEps(startingP)) { onNode = n; break; }
-    //         var startingNode:nb.Graph.Node = onNode;
-    //         var res:Array<Array<Point>> = [[onNode.p]];
-    //         var shapeC:Int = 0;
-    //         var vec = new Point(1,1);
-    //         var maxC:Int = 500;
-    //         while (1 == 1) {
-    //             unvisitedNodes.remove(onNode);
-    //             visitedNodes.push(onNode);
-    //             //trace(onNode.p);
-    //             var connectedNodes:Array<nb.Graph.Node> = [for (n in onNode.connections) n];
-    //             nb.ext.ArrayExt.quickSort(connectedNodes, (n1,n2) -> {
-    //                 // Todo: add to utils
-    //                 var vec = vec.normalized(); 
-    //                 var p1 = n1.p.sub(onNode.p).normalized(); var dp1 = vec.dot(p1);
-    //                 var p2 = n2.p.sub(onNode.p).normalized(); var dp2 = vec.dot(p2);
-    //                 var v1 = vec.cross(p1) >= 0 ? dp1 : (dp1*-1)-2;
-    //                 var v2 = vec.cross(p2) >= 0 ? dp2 : (dp2*-1)-2;
-    //                 return v1 > v2;
-    //             });
-    //             var a:Array<Point> = [];
-    //             for (node in connectedNodes) a.push(node.p.sub(onNode.p));
-    //             var s = "v:" + vec.normalized() + "  a:";
-    //             for (node in connectedNodes) s += "cross "+vec.normalized().cross(node.p.sub(onNode.p).normalized())+ "    dot " + vec.normalized().dot(node.p.sub(onNode.p).normalized()) + "    ";
-    //             //trace(a);
-    //             //trace(s);
-
-    //             var toNode = null;
-    //             for (node in connectedNodes) if (lastNode != node) { toNode = node; break; }
-
-    //             if (toNode != null) {
-    //                 vec = toNode.p.sub(onNode.p).multiply(-1);
-    //                 lastNode = onNode;
-    //                 onNode = toNode;
-    //                 res[shapeC].push(toNode.p);
-    //             }
-                
-    //             if (toNode == startingNode && res[shapeC].length > 2) {
-    //                 res[shapeC].pop();
-
-    //                 if (unvisitedNodes.length > 0) {
-    //                     nb.ext.ArrayExt.quickSort(unvisitedNodes, (n1,n2) -> {
-    //                         return n1.p.length() > n2.p.length();
-    //                     });
-
-    //                     var lastPol = new Polygon(res[shapeC]);
-    //                     var toRemove:Array<nb.Graph.Node> = [];
-    //                     for (node in unvisitedNodes) if (lastPol.containsPoint(node.p)) toRemove.push(node);
-    //                     for (node in toRemove) unvisitedNodes.remove(node);
-
-    //                     if (unvisitedNodes.length < 3) break;
-    //                     lastNode = null;
-    //                     startingNode = onNode = unvisitedNodes[0];
-    //                     vec = onNode.p.clone();
-    //                     shapeC++;
-    //                     res.push([onNode.p]);
-    //                 } else break;
-    //             }
-
-    //             if (--maxC <= 0) throw "union : max loop count reached";
-    //         }
-
-    //         // trace(res);
-    //         union = [for (a in res) if (a.length>=3) {
-    //             var pol = new h2d.col.Polygon(a.concat([a[0]])).optimize(0.15);
-    //             pol.points.pop();
-    //             new Polygon(pol.points,this);
-    //         }];
-    //         // trace(union);
-    //     } else {
-    //         union = shapes.copy();
-    //     }
-
-    // //    addChild(graph.debugDraw());
+                            for (seg2 in segments2) {
+                                var cInfo = seg1.checkSeg(seg2);
+                                if (cInfo != null) {
+                                    var p3 = seg2.getA();
+                                    var p4 = seg2.getB();
+                                    var nodeP3 = graph.getNodeAtPoint(p3);
+                                    var nodeP4 = graph.getNodeAtPoint(p4);
+                                    var nodeInters = graph.addNode(cInfo[0]);
+                                    graph.disconnect(nodeP3,[nodeP4]);
+                                    graph.connect(nodeInters,[nodeP1,nodeP2,nodeP3,nodeP4]);
+                                } else {
+                                    graph.connect(nodeP1,[nodeP2]);
+                                }
+                            }
+                        }
+                    }
+                }
+                checkedShapes.push(shape1);
+            }
+        }
+        return graph;
     }
 }
