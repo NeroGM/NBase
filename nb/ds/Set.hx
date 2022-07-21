4fe074011334222921286a7f8bda964d9c2fe797
NeroGM <Nero.GM@protonmail.com>
2022-07-21 08:32:26 +0200

    Documented nb.ds.Set

--- nb/ds/Set.hx
+++ nb/ds/Set.hx
@@ -6,20 +6,34 @@ import haxe.ds.IntMap;
 import haxe.ds.ObjectMap;
 import haxe.ds.EnumValueMap;
 
+/**
+ * Stores unique values without any particular order.
+ *
+ * @since 0.2.0
+ **/
 @:transitive
 @:multiType
 abstract Set<T>(Map<T,Bool>) {
+    /** Creates a new `nb.ds.Set`, which is an abstract defined over an `haxe.ds.Map`. **/
     public function new();
 
+    /**
+     * Adds an item to the set.
+     *
+     * @param item The value to add.
+     * @return `true` if it's a new value, `false` otherwise.
+     **/
     public inline function add(item:T):Bool {
         if (this.exists(item)) return false;
         return this[item] = true;
 	}
 
+    /** Returns an iterator over the values of this set. The order is undefined. **/
     public inline function iterator():Iterator<T> {
         return this.keys();
     }
 
+    /** Returns the string representation of this set. **/
     public inline function toString():String {
         var s:String = "{";
         var it = iterator();
@@ -31,10 +45,12 @@ abstract Set<T>(Map<T,Bool>) {
         return s;
     }
 
+    /** Returns an array containing the values of this set. The order is undefined. **/
     public inline function toArray() {
         return [for (v in iterator()) v];
     }
 
+    /** Removes all items in this set. **/
     public inline function clear() {
         this.clear();
     }
