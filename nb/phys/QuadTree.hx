a6c207a83c7100232463e2ea42ad52250d560610
NeroGM <Nero.GM@protonmail.com>
2022-07-31 07:29:39 +0200

    QuadTree: another constructors patch

--- nb/phys/QuadTree.hx
+++ nb/phys/QuadTree.hx
@@ -21,15 +21,18 @@ class Quad extends Rectangle {
     public var maxBucketSize:Int = 4;
 
     override private function new(x:Float, y:Float, w:Float, h:Float, quadTree:QuadTree, ?parentQuad:Quad) {
-        super(0,0,w,h,quadTree);
+        super(0,0,w,h,parentQuad);
         setPosition(x,y);
         this.quadTree = quadTree;
-        this.parentQuad = parentQuad == null ? quadTree : parentQuad;
+
+        if (parentQuad == null) return;
+        this.parentQuad = parentQuad;
+        this.parentQuad.childQuads.push(this);
     }
 }
 
 class QuadTree extends Quad {
     override public function new(x:Float, y:Float, w:Float, h:Float, ?parent:Object) {
-        super(x,y,w,h,null);
+        super(x,y,w,h,this,null);
     }
 }
